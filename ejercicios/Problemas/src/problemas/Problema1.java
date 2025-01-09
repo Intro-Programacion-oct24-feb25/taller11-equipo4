06a5261b543a51f80e880c91ee5ccbc302ea022c
Dalton Flores <daltonflores@gmail.com>
2025-01-09 11:31:59 -0500

    ej1

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -22,7 +22,7 @@ public class Problema1 {
     public static void imprimirDatos(int[][] datos) {
         String cadena = "";
         for (int i = 0; i < datos.length; i++) {
-            for (int j = 0; j < datos.length; i++) {
+            for (int j = 0; j < datos.length; j++) {
 
                 if (datos[i][j] % 2 == 0) {
                     cadena = String.format("%s%d\n", cadena, datos[i][j]);
